b78092a3cfc40b34cb8021d0065ade24ecae4952
Vnalvarado <157657964+Vnalvarado@users.noreply.github.com>
2024-02-21 15:52:31 -0800

    something

---
+++ for_loops.py
@@ -0,0 +1,19 @@
+print("Options:1,2,3,4,5,6,7,8,9,10")
+run = input("Enter the problem you want to run:")
+run=int(run)
+if run ==1: 
+    print("Running Problem 1")
+    for count in range (1,1001):
+    print(count)
+elif run ==2:
+    print("Running problem 2")
+    for count in range(1,1001,2):
+        print(count)
+elif run ==3: 
+    print("Running problem 3")
+    for count in range(1,1001)
+    if run % 6 == 0:
+        print(count)
+elif run ==4: 
+    print("Running problem 4")
+    for count in range (1,1001)
